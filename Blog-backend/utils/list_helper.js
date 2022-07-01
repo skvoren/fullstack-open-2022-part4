d6c1caff29af878922468c721467cdf6fda70822
skvoren <skvoren@outlook.com>
2022-07-01 23:15:28 +0400

    exercise 4.4

--- Blog-backend/utils/list_helper.js
+++ Blog-backend/utils/list_helper.js
@@ -4,6 +4,15 @@ const dummy = (blogs) => {
     }
 }
 
+const likes = (blogs) => {
+    let result = 0
+    if (blogs) {
+        blogs.map(blog => result += blog.likes)
+    }
+    return result
+}
+
 module.exports = {
-    dummy
+    dummy,
+    likes
 }

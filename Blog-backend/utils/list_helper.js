5deca855eaaa90a625501309b62525bc90d8e4de
skvoren <skvoren@outlook.com>
2022-07-03 13:38:03 +0400

    exercise 4.6

--- Blog-backend/utils/list_helper.js
+++ Blog-backend/utils/list_helper.js
@@ -6,13 +6,41 @@ const dummy = (blogs) => {
 
 const likes = (blogs) => {
     let result = 0
-    if (blogs) {
-        blogs.map(blog => result += blog.likes)
+    blogs.map(blog => result += blog.likes)
+    return result
+}
+
+const favoriteBlog = (blogs) => {
+    let result
+    let maxLikes = 0
+    blogs.map(blog => {
+        if (blog.likes > maxLikes) {
+            result = blog
+            maxLikes = blog.likes
+        }
+    })
+    return result
+}
+
+const mostBlogs = (blogs) => {
+    let result = {
+        author: '',
+        blogs: 0
     }
+    let blogsForFilter = blogs
+    blogs.forEach(blog => {
+        let countOfBlogs = blogsForFilter.filter(blogItem => blogItem.author === blog.author).length
+        if (countOfBlogs > result.blogs) {
+            result.author = blog.author
+            result.blogs = countOfBlogs
+        }
+    })
     return result
 }
 
 module.exports = {
     dummy,
-    likes
+    likes,
+    favoriteBlog,
+    mostBlogs
 }

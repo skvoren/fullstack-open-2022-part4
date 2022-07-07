9fd65543a4c68df39014c0b4b6fc328e99c8c05e
skvoren <skvoren@outlook.com>
2022-07-08 01:21:24 +0400

    exercise 4.17

--- Blog-backend/controllers/blogs.js
+++ Blog-backend/controllers/blogs.js
@@ -1,22 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/blogs', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
 	response.json(blogs)
 })
 
 blogsRouter.post('/blogs', async (request, response) => {
 	const body = request.body
+	const user = await User.findById(body.userId)
 
 	const blog = new Blog({
 		author: body.author,
 		title: body.title,
 		url: body.url,
 		likes: body.likes,
+		user: user._id
 	})
 
 	const savedBlog = await blog.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await blog.save()
+
 	response.status(201).json(savedBlog)
 })
 
